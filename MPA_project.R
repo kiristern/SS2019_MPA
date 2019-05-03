fb93e09f126a817ac6142bad4e9b9498423a5c12
Kiri Stern <kiri.a.stern@gmail.com>
2019-05-03 15:22:52 -0400

    upload data

--- MPA_project.R
+++ MPA_project.R
@@ -1,7 +1,9 @@
 library("tidyverse")
+library("ggplot2")
 
-read.csv("Coastline_Length_KM.csv")
-read.csv("DOxy_SJ_Mean.csv")
-read.csv("PProd_SJ_Mean.csv")
-read.csv("SST_SJ_Mean.csv")
-read.csv("Fish_Rich_SJ.csv")
+Coast <- read.csv("Coastline_Length_KM.csv", header = TRUE)
+DOxy <- read.csv("DOxy_SJ_Mean.csv", header = TRUE)
+PProd <- read.csv("PProd_SJ_Mean.csv", header = TRUE)
+SeaTemp <- read.csv("SST_SJ_Mean.csv", header = TRUE)
+Fish <- read.csv("Fish_Rich_SJ.csv", header = TRUE)
+
