62fda5b15975cd89929bbc14c291dbfe59013e49
Kiri Stern <kiri.a.stern@gmail.com>
2019-05-04 14:07:09 -0400

    final

--- all_variables_matrix.R
+++ all_variables_matrix.R
@@ -9,8 +9,23 @@ final_table <- final_table %>%
   mutate(Pres_Abs = 1)
 View(final_table)
 
-#make pivot table of Grid cell ~ species 
-pt <- dcast(final_table, FID ~ BINOMIAL, value.var = "Pres_Abs")
+#create columns for each species
+sp_col <- dcast(final_table, FID ~ BINOMIAL, value.var = "Pres_Abs")
 
 #delete empty column from pivot table
-pt <- pt[,-(2)] 
+sp_col_final <- sp_col[,-(2)] 
+
+#merge all variables into their proper FID
+final_table <- final_table[,-c(1,10:11)] #remove species column
+
+final_table_distinct <- final_table %>% glimpse %>% 
+  distinct  #combine all rows with same value
+
+final_table_distinct %>% glimpse
+
+#final_table[!duplicated(final_table), ] ##old school way of doing distinct function
+
+#join enviro factor columns to pivot table
+all_data_table <- merge(final_table_distinct, sp_col_final, by = "FID")
+View(all_data_table)
+write.csv(all_data_table, "all_data_table.csv")
