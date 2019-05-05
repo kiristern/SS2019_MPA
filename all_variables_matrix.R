8f64910a644ec516fe5a0e45935690967bffcc1b
Kiri <kiri.a.stern@gmail.com>
2019-05-04 20:17:30 -0400

    add column with total species richness, delete column Pres_Abs remove fish matrix

--- all_variables_matrix.R
+++ all_variables_matrix.R
@@ -23,29 +23,34 @@ final_table_distinct <- final_table %>% glimpse %>%
 
 final_table_distinct %>% glimpse
 
-#final_table[!duplicated(final_table), ] ##old school way of doing distinct function
+    #final_table[!duplicated(final_table), ] ##old school way of doing distinct function
 
-#join enviro factor columns to pivot table
+#join enviro factor columns to species columns table
 all_data_table <- merge(final_table_distinct, sp_col_final, by = "FID")
 View(all_data_table)
 #write.csv(all_data_table, "all_data_table.csv")
 
 # renaming data table
-fish <- all_data_table
-head(fish)
+# fish <- all_data_table
+# head(fish)
 
-# creating cells by species matrix for ordination
-fishmatrix <- as.matrix(fish[,17:294])
-dim(fishmatrix) # 3078 cells by 278 species
-table(fishmatrix)
-
-# set all values >=1 to 1
-fishmatrix[which(fishmatrix > 1)] <- 1
+# # creating cells by species matrix for ordination
+# fishmatrix <- as.matrix(fish[,17:294])
+# dim(fishmatrix) # 3078 cells by 278 species
+# table(fishmatrix)
+# 
+# # set all values >=1 to 1
+# fishmatrix[which(fishmatrix > 1)] <- 1
 
 #add column of MPA presence
 all_data_table$MPA_presence <- ifelse(all_data_table$WDPA_PID != " ", yes = NA, no = "MPA")
 count(all_data_table, all_data_table$MPA_presence)        
 
-#delete column WDPA_PID
-all_data_table <- all_data_table[,-(WDPA_PID)] 
+#delete column WDPA_PID and Pres_Abs
+all_data_table <- all_data_table[,-c(WDPA_PID, Pres_Abs)] 
 View(all_data_table)
+
+#add column with total species richness
+all_data_table <- all_data_table %>%
+  mutate(richness=rowSums(all_data_table[17:294]))
+glimpse(all_data_table)
